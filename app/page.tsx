2e4f9de9e71c5d9c2575a9c22961c7723fa22e6c
hariskhanweb <haris071@gmail.com>
2026-01-05 16:43:38 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { Suspense } from 'react'
 import ChatInterface from '@/components/ChatInterface'
 
 export default function Home() {
   return (
     <main className="h-screen">
-      <ChatInterface />
+      <Suspense fallback={
+        <div className="h-screen flex items-center justify-center bg-[#0B141A]">
+          <div className="text-white">Loading...</div>
+        </div>
+      }>
+        <ChatInterface />
+      </Suspense>
     </main>
   )
 }
